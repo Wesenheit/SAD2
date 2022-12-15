9213177073adf6617b10dc384cde96d9a2719f63
Wesenheit <matio@opoczta.pl>
2022-12-15 01:32:06 +0100

    projekt nr 1

--- Project1/src/VAE.py
+++ Project1/src/VAE.py
@@ -4,17 +4,41 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import seaborn as sns
 from torchvision.utils import save_image
-sns.set_theme()
-torch.set_default_dtype(torch.float32)
+import pyro
+from pyro import distributions as dst
+
+
+class log1p_layer(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self,x):
+        return torch.log1p(x)
+
+
+class norm_layer(nn.Module):
+    def __init__(self,mean,std):
+        super().__init__()
+        self.mean=mean
+        self.std=std
+    def forward(self,x):
+        return (x-self.mean)/self.std
 
 class Encoder(nn.Module):
-    def __init__(self,sizes):
+    def __init__(self,sizes,preproces=None,gelu=False):
         super().__init__()
         self.latent_size=sizes[-1]
         self.network=[]
+        if preproces is not None:
+            self.network.append(preproces)
         for i in range(len(sizes)-2):
             self.network.append(nn.Linear(sizes[i],sizes[i+1]))
-            self.network.append(nn.ReLU())
+            self.network.append(nn.BatchNorm1d(sizes[i+1]))
+            if gelu:
+                self.network.append(nn.GELU())
+            else:
+                self.network.append(nn.ReLU())
+            self.network.append(nn.Dropout(p=0.05))
         self.encode_mu=nn.Linear(sizes[-2],sizes[-1])
         self.encode_sigma=nn.Linear(sizes[-2],sizes[-1])
         self.network=nn.Sequential(*self.network)
@@ -22,87 +46,211 @@ class Encoder(nn.Module):
     def forward(self,x):
         x=self.network(x)
         mu=self.encode_mu(x)
-        sigma=torch.exp(self.encode_sigma(x))
+        sigma=nn.functional.relu(self.encode_sigma(x))+1e-5
         return mu,sigma
 
 
 
 class Decoder(nn.Module):
-    def __init__(self,sizes):
+    def __init__(self,sizes,gelu=False):
         super().__init__()
         self.network=[]
         for i in range(len(sizes)-2):
             self.network.append(nn.Linear(sizes[i],sizes[i+1]))
-            self.network.append(nn.ReLU())
+            self.network.append(nn.BatchNorm1d(sizes[i+1]))
+            if gelu:
+                self.network.append(nn.GELU())
+            else:
+                self.network.append(nn.ReLU())
+            self.network.append(nn.Dropout(p=0.05))
         self.network.append(nn.Linear(sizes[-2],sizes[-1]))
         self.network=nn.Sequential(*self.network)
 
     def forward(self,x):
-        return torch.sigmoid(self.network(x))
+        return self.network(x)
 
-class VAE_torch(nn.Module):
-    def __init__(self,encoder, decoder,latent_size):
+class Decoder_Ext(nn.Module):
+    def __init__(self,sizes,gelu=False):
         super().__init__()
-        self.latent_size=latent_size
-        self.encoder=encoder
-        self.decoder=decoder
-    
+        self.network=[]
+        for i in range(len(sizes)-2):
+            self.network.append(nn.Linear(sizes[i],sizes[i+1]))
+            self.network.append(nn.BatchNorm1d(sizes[i+1]))
+            if gelu:
+                self.network.append(nn.GELU())
+            else:
+                self.network.append(nn.ReLU())
+            self.network.append(nn.Dropout(p=0.05))
+        self.network=nn.Sequential(*self.network)
+        self.output=nn.Linear(sizes[-2],sizes[-1])
+        self.param=nn.Linear(sizes[-2],sizes[-1])
+
     def forward(self,x):
-        mu,sigma=self.encoder(x)
-        eps=torch.normal(0,1,(mu.shape[0],self.latent_size))
-        sampled_vector=mu+eps*sigma
-        return self.decoder(sampled_vector),mu,sigma
+        x=self.network(x)
+        return self.output(x),self.param(x)
 
-    def sample(self,x,epsilon=0.0):
-        mu,sigma=self.encoder(x)
-        sampled_vector=torch.zeros(x.shape[0],self.latent_size)
-        for j in range(mu.shape[0]):
-            sampled_vector[j,:]=torch.distributions.Normal(mu[j],sigma[j]).sample()
-        print(sampled_vector)
-        return self.decoder(sampled_vector)
+class TemplateVAE(nn.Module):
+    def __init__(self,sizes1,sizes2,device="cpu",ext=False,preprocess=None,gelu=False):
+        super().__init__()
+        self.encoder=Encoder(sizes1,preproces=preprocess).to(device)
+        if ext:
+            self.decoder=Decoder_Ext(sizes2,gelu=gelu).to(device)
+        else:
+            self.decoder=Decoder(sizes2,gelu=gelu).to(device)
+        self.latent_size=sizes1[-1]
+        self.device=device
 
+    def model(self,data):
+        pass
 
-def KL(mean,sigma):
-    return 0.5*torch.sum((torch.sum(sigma.pow(2),dim=1)+torch.sum(mean.pow(2),dim=1)-torch.sum(torch.log(sigma.pow(2)),dim=1)-1))
+    def guide(self,data):
+        pyro.module("encoder",self.encoder)
+        with pyro.plate("data",data.shape[0]):
+            mu,sigma=self.encoder(data)
+            #print(torch.max(sigma))
+            pyro.sample("latent",dst.Normal(mu,sigma).to_event(1))
 
-def ELBO(values,pred_values,mean,sigma,beta=1):
-    return (nn.functional.binary_cross_entropy(pred_values, values,reduction="sum")+beta*KL(mean,sigma))/values.shape[0]
-
-def ELBO_MSE(values,pred_values,mean,sigma,beta=1):
-    return (nn.functional.mse_loss(pred_values,values,reduction="sum")+beta*KL(mean,sigma))/values.shape[0]
-
-def visualize(res1,VAE):
-    new=VAE.sample(res1.expand(5,784))
-    f,axes=plt.subplots(2,3,figsize=(21,14))
-    ax=axes.flatten()[0]
-    ax.imshow(res1.reshape(28,28),cmap="viridis_r")
-    ax.set_title("Original image")
-    for spine in ax.spines.values():
-        spine.set_edgecolor('red')
-        spine.set_linewidth(4)
-    for j in range(5):
-        ax=axes.flatten()[j+1]
-        ax.imshow(new[j,:].detach().numpy().reshape(28,28),cmap="viridis_r")
-    plt.tight_layout()
-
-
-def load_mnist(path='mnist.npz'):
-    with np.load(path) as f:
-        x_train, _y_train = f['x_train'], f['y_train']
-        x_test, _y_test = f['x_test'], f['y_test']
+    def sample(self,data,n_sample):
+        samples=torch.zeros(data.shape[0],n_sample,self.latent_size,device=self.device)
+        with torch.no_grad():
+            mu,sigma=self.encoder(data)
+            for i in range(n_sample):
+                samples[:,i,:]=mu+sigma*torch.normal(0,1,mu.shape,device=self.device)
+        return samples.view(data.shape[0]*n_sample,self.latent_size)
+    
+    def embed(self,data):
+        with torch.no_grad():
+            mu,sigma=self.encoder(data)
+            return mu
         
-    x_train = x_train.reshape(-1, 28 * 28) / 255.
-    x_test = x_test.reshape(-1, 28 * 28) / 255.
-    
-    y_train = np.zeros((_y_train.shape[0], 10))
-    y_train[np.arange(_y_train.shape[0]), _y_train] = 1
-    
-    y_test = np.zeros((_y_test.shape[0], 10))
-    y_test[np.arange(_y_test.shape[0]), _y_test] = 1
+    def kl_loss(self,data):
+        with torch.no_grad():
+            mu,sigma=self.encoder(data)
+            return 0.5*torch.sum(mu.pow(2)+sigma.pow(2)-torch.log(sigma.pow(2))-1)/data.shape[0]   
 
-    return (torch.tensor(x_train,dtype=torch.float32), torch.tensor(y_train,dtype=torch.float32)), (torch.tensor(x_test,dtype=torch.float32),torch.tensor(y_test,dtype=torch.float32))
+    def save(self,name="template_VAE.tc"):
+        torch.save(self, name)
 
-def generate(VAE,n,name="images.png"):
-    input=torch.normal(0,1,[n*n,VAE.latent_size])
-    output=VAE.decoder(input)
-    save_image(output.reshape(n*n,1,28,28),name)
+
+class GaussianVAE(TemplateVAE):
+    def __init__(self,sizes1,sizes2,device="cpu",scale=1,preprocess=None,gelu=False):
+        super().__init__(sizes1,sizes2,device,preprocess=preprocess,gelu=gelu)
+        self.scale=scale
+    def model(self,data):
+        pyro.module("decoder",self.decoder)
+        with pyro.plate("data",data.shape[0]):
+            mu = torch.zeros(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            sigma = torch.ones(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+
+            z_latent=pyro.sample("latent",dst.Normal(mu,sigma).to_event(1))
+            mean=nn.functional.relu(self.decoder(z_latent))
+            pyro.sample("observation",dst.Normal(loc=mean,scale=self.scale).to_event(1),obs=data)
+    def save(self,name="vanila_VAE.tc"):
+        torch.save(self, name)
+
+class NegBinomialVAE(TemplateVAE):
+    def __init__(self,sizes1,sizes2,device="cpu",preprocess=None,gelu=False):
+        super().__init__(sizes1,sizes2,device,ext=True,preprocess=preprocess,gelu=gelu)
+    def model(self,data):
+        pyro.module("decoder",self.decoder)
+        with pyro.plate("data",data.shape[0]):
+            mu = torch.zeros(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            sigma = torch.ones(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            z_latent=pyro.sample("latent",dst.Normal(mu,sigma).to_event(1))
+            total_count,logits=self.decoder(z_latent)
+            
+            pyro.sample("observation",dst.NegativeBinomial(total_count=nn.functional.relu(total_count)+1e-5,logits=logits).to_event(1),obs=data.int())
+    def save(self,name="NegBinom_VAE.tc"):
+        torch.save(self, name)
+
+
+class ZeroPoissVAE(TemplateVAE):
+    def __init__(self,sizes1,sizes2,device="cpu",preprocess=None):
+        super().__init__(sizes1,sizes2,device,preprocess=preprocess)
+        self.tot_count=nn.Linear(self.latent_size,sizes2[-1]).to(self.device)
+    def model(self,data):
+        pyro.module("decoder",self.decoder)
+        pyro.module("decoder",self.tot_count)
+        with pyro.plate("data",data.shape[0]):
+            mu = torch.zeros(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            sigma = torch.ones(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            z_latent=pyro.sample("latent",dst.Normal(mu,sigma).to_event(1))
+            logs=self.decoder(z_latent)
+            total_count=self.tot_count(z_latent)
+            pyro.sample("observation",dst.ZeroInflatedPoisson(gate_logits=total_count,rate=nn.functional.relu(logs)+1e-6).to_event(1),obs=data.int())
+    def save(self,name="NegBinom_VAE.tc"):
+        torch.save(self, name)
+
+
+class NegBinomBatchVAE(TemplateVAE):
+    def __init__(self,sizes1,sizes2,batch,device="cpu",preprocess=None,gelu=False):
+        temp=sizes2
+        temp[0]+=batch
+        super().__init__(sizes1,temp,device,ext=True,preprocess=preprocess,gelu=gelu)
+    def model(self,data,batch):
+        pyro.module("decoder",self.decoder)
+        with pyro.plate("data",data.shape[0]):
+            mu = torch.zeros(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            sigma = torch.ones(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            z_latent=pyro.sample("latent",dst.Normal(mu,sigma).to_event(1))
+            new=torch.cat((z_latent,batch),dim=1)
+            total_count,logits=self.decoder(new)
+            
+            pyro.sample("observation",dst.NegativeBinomial(total_count=nn.functional.relu(total_count)+1e-5,logits=logits).to_event(1),obs=data.int())
+
+    def guide(self,data,batch):
+        pyro.module("encoder",self.encoder)
+        with pyro.plate("data",data.shape[0]):
+            mu,sigma=self.encoder(data)
+            pyro.sample("latent",dst.Normal(mu,sigma).to_event(1))
+    def save(self,name="NegBinomBatch_VAE.tc"):
+        torch.save(self, name)
+
+class ZeroPoissBatchVAE(TemplateVAE):
+    def __init__(self,sizes1,sizes2,batch,device="cpu",preprocess=None):
+        temp=sizes2
+        temp[0]+=batch
+        super().__init__(sizes1,temp,device,ext=True,preprocess=preprocess)
+    def model(self,data,batch):
+        pyro.module("decoder",self.decoder)
+        with pyro.plate("data",data.shape[0]):
+            mu = torch.zeros(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            sigma = torch.ones(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            z_latent=pyro.sample("latent",dst.Normal(mu,sigma).to_event(1))
+            new=torch.cat((z_latent,batch),dim=1)
+            total_count,logits=self.decoder(new)
+            
+            pyro.sample("observation",dst.ZeroInflatedPoisson(rate=nn.functional.relu(total_count)+1e-5,gate_logits=logits).to_event(1),obs=data.int())
+
+    def guide(self,data,batch):
+        pyro.module("encoder",self.encoder)
+        with pyro.plate("data",data.shape[0]):
+            mu,sigma=self.encoder(data)
+            pyro.sample("latent",dst.Normal(mu,sigma).to_event(1))
+    def save(self,name="ZeroPoissBatch_VAE.tc"):
+        torch.save(self, name)
+
+
+class GaussianBatchVAE(TemplateVAE):
+    def __init__(self,sizes1,sizes2,batch,device="cpu"):
+        temp=sizes2
+        temp[0]+=batch
+        super().__init__(sizes1,temp,device)
+    def model(self,data,batch):
+        pyro.module("decoder",self.decoder)
+        with pyro.plate("data",data.shape[0]):
+            mu = torch.zeros(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            sigma = torch.ones(torch.Size((data.shape[0], self.latent_size)),device=self.device)
+            z_latent=pyro.sample("latent",dst.Normal(mu,sigma).to_event(1))
+            new=torch.cat((z_latent,batch),dim=1)
+            mean=self.decoder(new)
+            
+            pyro.sample("observation",dst.Normal(loc=mean,scale=1).to_event(1),obs=data)
+
+    def guide(self,data,batch):
+        pyro.module("encoder",self.encoder)
+        with pyro.plate("data",data.shape[0]):
+            mu,sigma=self.encoder(data)
+            pyro.sample("latent",dst.Normal(mu,sigma).to_event(1))
+    def save(self,name="GaussianBatch_VAE.tc"):
+        torch.save(self, name)
